a99375b72bc2cec64a0ede1b15c755b70eddfca8
we sell insurance <neelesh.poli2006@gmail.com>
2023-03-14 09:12:26 -0500

    Fixed issure with mob health scaling working on players.

--- src/main/java/com/fixthegame/mixin/ScaleMobHealth.java
+++ src/main/java/com/fixthegame/mixin/ScaleMobHealth.java
@@ -7,13 +7,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.fixthegame.FixTheGame;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+
 /* 
  * This class increases the health of all mobs.
  */
-@Mixin(net.minecraft.entity.LivingEntity.class)
+@Mixin(LivingEntity.class)
 public class ScaleMobHealth {
     @Inject(method = "getMaxHealth", at = @At("RETURN"), cancellable = true)
     private void getMaxHealth(CallbackInfoReturnable<Float> info) {
-        info.setReturnValue(info.getReturnValueF() * FixTheGame.scalehealth);
+        if (!(((LivingEntity)(Object)this) instanceof PlayerEntity)) info.setReturnValue(info.getReturnValueF() * FixTheGame.scalehealth);
+        info.setReturnValue(info.getReturnValueF());
     }
 }
