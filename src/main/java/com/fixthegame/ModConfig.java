f59b82ac313986e924a15259482d193f76fc258f
we sell insurance <neelesh.poli2006@gmail.com>
2023-03-14 08:35:13 -0500

    Added new features and cleaned config file

--- src/main/java/com/fixthegame/ModConfig.java
+++ src/main/java/com/fixthegame/ModConfig.java
@@ -9,9 +9,18 @@ public class ModConfig extends Config{
         super(ConfigOptions.mod(FixTheGame.MODID));
     }
 
-    @ConfigEntry(comment = "This is the config file for Nerf the game.\nALL FEATURES ARE DISABLED BY DEFAULT!\nEnable the features you would like to\n\nDisables villager breeding, but villagers will continue\nto pick up food. ")
+    @ConfigEntry(comment = "Disables villager breeding, but villagers will continue to pick up food. ")
     boolean disableVillagerBreeding = false;
 
     @ConfigEntry(comment = "Scales the health of all mobs (not including the player) by specified value.")
+    @ConfigEntry.BoundedFloat(min = 0)
     float scalehealth = 1;
+
+    @ConfigEntry(comment = "Sets the amount to slow down crop growth by, as a decimal (not a percent!). Set this to less than 1 in order to slow down the crop speed.")
+    @ConfigEntry.BoundedFloat(min = 0)
+    float cropGrowthSlownessMultiplier = 1;
+
+    @ConfigEntry(comment = "Scales the amount of time it takes for a baby passive mob to grow.")
+    @ConfigEntry.BoundedFloat(min = 0)
+    float scaleBabyTime = 1;
 }
